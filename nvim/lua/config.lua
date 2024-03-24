4a0cc11629ec6822acbcd7a5f9e0cb5a754ab60d
Jay Howard <hello@jaydanhoward.com>
2024-03-23 21:47:55 -0700

    use init lua

---
+++ nvim/lua/config.lua
@@ -0,0 +1,139 @@
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local uv = vim.uv or vim.loop
+
+-- Auto-install lazy.nvim if not present
+if not uv.fs_stat(lazypath) then
+  print('Installing lazy.nvim....')
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+  print('Done.')
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+    {'L3MON4D3/LuaSnip'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'drewtempelmeyer/palenight.vim'},
+    {'elzr/vim-json'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-cmdline'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-path'},
+    {'hrsh7th/vim-vsnip'},
+    {'hrsh7th/nvim-cmp'},
+    {'jiangmiao/auto-pairs'},
+    {'lilydjwg/colorizer'},
+    {'lukas-reineke/indent-blankline.nvim'},
+    {'neovim/nvim-lspconfig'},
+    {'nvim-lua/plenary.nvim'},
+    {'nvim-telescope/telescope.nvim'},
+    {'nvim-tree/nvim-web-devicons'},
+    {'rust-lang/rust.vim'},
+    {'tpope/vim-fugitive'},
+    {'tpope/vim-surround'},
+    {'tpope/vim-vinegar'},
+    {'rafamadriz/friendly-snippets'}
+})
+
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+local nvim_lsp = require'lspconfig'
+
+nvim_lsp.rust_analyzer.setup({
+    capabilities = capabilities,
+    on_attach=on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+})
+
+nvim_lsp.tsserver.setup {
+    capabilities = capabilities
+}
+
+nvim_lsp.gopls.setup({
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+      gofumpt = true,
+    },
+  },
+})
+
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+local cmp_format = require('lsp-zero').cmp_format({details = true})
+
+cmp.setup({
+    snippet = {
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body)
+      end,
+    },
+    window = {},
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),}),
+
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, 
+      { name = 'buffer' },
+    })
+})
+
+cmp.setup.filetype('gitcommit', {
+    sources = {
+      { name = 'git' },
+      { name = 'buffer' },
+    }
+})
+
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+})
+
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'path' },
+      { name = 'cmdline' },
+    }
+})
+
